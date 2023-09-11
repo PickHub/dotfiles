93d36be504ac8a2783093fbc4926375a52727e98
Daniel Handloser <pickhub@github.com>
2023-09-11 10:32:22 +0200

    Add git ignore to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo '[[ -v GH_GH_PAT ]] && export GITHUB_TOKEN=$GH_GH_PAT' >> ~/.bashrc
+
+# Git ignore
+echo "config.json" >> ~/.gitignore_global
+echo ".vscode/settings.json" >> ~/.gitignore_global
+echo ".env" >> ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
